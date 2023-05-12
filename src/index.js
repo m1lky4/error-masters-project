aec92f7b3a75048b23dfa2dbc3446e80496975ad
m1lky4 <temanafanin1337@gmail.com>
2023-05-12 15:49:58 +0200

    added starter files

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './js/for-each-partial/book-list';
+import './js/for-each-partial/categories';
+import './js/for-each-partial/header';
+import './js/for-each-partial/modal';
+import './js/for-each-partial/store';
+import './js/for-each-partial/support';
