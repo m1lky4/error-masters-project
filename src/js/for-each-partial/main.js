3fd2f460578e15fc269a26b8f1e9ca6e3c8a924e
Artem Kozlov <125802820+m1lky4@users.noreply.github.com>
2023-05-13 17:13:14 +0200

    added book list (#17)
    * added starter files
    
    * fixed pakage.json
    
    * fixed path
    
    * deleted shopping list from index.html
    
    * Update _common.scss (#5)
    
    * fixed container
    
    * finished requests (#8)
    
    * O.d. book list markup (#10)
    
    * create render func + css
    
    1. func render booklist
    2. css styles
    
    * Update package-lock.json
    
    * create-markup-book-card (#11)
    
    Co-authored-by: Artem Kozlov <125802820+m1lky4@users.noreply.github.com>
    
    * removing unnecessary code (#13)
    
    * Create markup (#15)
    
    * render-markup-on-html
    
    * fix-container-sccs
    
    * O.d. book list markup (#16)
    
    * create render func + css
    
    1. func render booklist
    2. css styles
    
    * Update package-lock.json
    
    * create render book cards func
    
    ---------
    
    Co-authored-by: Ch1mik <125959505+Ch1mik@users.noreply.github.com>
    Co-authored-by: Misha Hudz <42496251+MishaHudz@users.noreply.github.com>
    Co-authored-by: Oleksandr Dubovyi <125696737+sashadubovyi@users.noreply.github.com>
    Co-authored-by: Vladyslav <124445034+VladyslavTodo@users.noreply.github.com>

---
+++ src/js/for-each-partial/main.js
@@ -0,0 +1,29 @@
+import { createTopBookList } from './book_list_markup';
+import { BookAPI } from '../api/book.service';
+import { formatResponse } from '../helpers/format_data';
+import debounce from 'lodash.debounce';
+
+export let data;
+const bookAPI = new BookAPI();
+
+export function renderTopBookList(data, limit) {
+  const markup = createTopBookList(data, limit);
+  const categoryMarkupList = document.querySelector('.category-markup-list');
+  categoryMarkupList.innerHTML = markup;
+}
+
+window.addEventListener('resize', debounce(formatResponse, 300));
+
+window.addEventListener('load', async e => {
+  data = await bookAPI.getTopBooksList();
+
+  if (!data) return console.log('Error');
+
+  if (window.innerWidth < 767) {
+    renderTopBookList(data, 1);
+  } else if (window.innerWidth >= 767 && window.innerWidth <= 1199) {
+    renderTopBookList(data, 3);
+  } else if (window.innerWidth >= 1200) {
+    renderTopBookList(data, 5);
+  }
+});
