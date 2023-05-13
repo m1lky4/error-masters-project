69b7994d6b2b4206cb6b9db49b7c8fe67ec97b51
sashatrnn <125980974+sashatrnn@users.noreply.github.com>
2023-05-13 20:40:06 +0300

    nothing

--- src/js/for-each-partial/modal.js
+++ src/js/for-each-partial/modal.js
@@ -1,8 +1,20 @@
+import {BookAPI} from '../api/book.service'
+const example = new BookAPI();
+
 const body = document.body;
 const modal = document.querySelector('.modal');
 const backdrop = document.querySelector('.backdrop');
 const modalOpenBtn = document.querySelector('button[data-modal-open-btn]');
 const modalCloseBtn = document.querySelector('button[data-modal-close-btn]');
+const modalImg = document.querySelector('.modal-img');
+const modalText = document.querySelector('.modal-text');
+const modalShopList = document.querySelector('.modal-shop-list');
+const shoppingBtn = document.querySelector('.modal-shopping-btn');
+const congratulationsText = document.querySelector('.congratulations-text');
+
+// let shoppingList = [];
+let shoppingBook = {};
+let bookId = null;
 
 function openModal() {
   backdrop.classList.remove('is-hidden');
@@ -35,3 +47,81 @@ function closeModalOnEsc(event) {
 }
 
 modalOpenBtn.addEventListener('click', openModal);
+
+// ================================================
+
+modalOpenBtn.addEventListener('click', async (e) => {
+  // const bookID = 'YOUR_BOOK_ID_HERE'; // Замініть на фактичний ідентифікатор книги
+  if (e.target.nodeName === 'IMG') {
+    bookId = e.target.parentNode.parentNode.parentNode.id;
+  } else if (e.target.nodeName === 'A') {
+    bookId = e.target.parentNode.id;
+  } else {
+    bookId = e.target.parentNode.parentNode.id;
+  }
+
+  try {
+    const bookData = await example.getBookByID(bookID);
+
+    // Оновлення модального вікна з отриманими даними
+    modalImg.innerHTML = `<img src="${bookData.image}" alt="${bookData.title}" />`;
+    modalText.innerHTML = `
+      <h3>${bookData.title}</h3>
+      <p>Автор: ${bookData.author}</p>
+      <p>${bookData.description}</p>
+    `;
+
+    // Оновлення посилань на торговельні майданчики
+    modalShopList.innerHTML = `<li>
+    <a href="${bookData.shopLinks.amazon}" class="modal-shop-link" target="_blank">
+      <img src="./images/modal-images/amazon.png" alt="logo">
+    </a>
+  </li>
+  <li>
+    <a href="${bookData.shopLinks.appleBook}" class="modal-shop-link" target="_blank">
+      <img src="./images/modal-images/appleBook.png" alt="logo">
+    </a>
+  </li>
+  <li>
+    <a href="${bookData.shopLinks.bookShop}" class="modal-shop-link" target="_blank">
+      <img src="./images/modal-images/bookShop.png" alt="logo">
+    </a>
+  </li>
+`;
+
+shoppingBook = {
+  id: bookID,
+  title: bookData.title,
+  author: bookData.author
+};
+openModal();
+} catch (error) {
+console.error('Помилка отримання даних про книгу', error);
+}
+});
+
+// shoppingBtn.addEventListener('click', addToShoppingList);
+
+// // Функція додавання книги до списку покупок
+// function addToShoppingList() {
+// if (shoppingBook.id) {
+// shoppingList.push(shoppingBook);
+// congratulationsText.textContent = 'Книга додана до списку покупок!';
+// saveShoppingList();
+// }
+// }
+
+// // Функція збереження списку покупок в локальне сховище
+// function saveShoppingList() {
+// localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
+// }
+
+// // Функція завантаження списку покупок з локального сховища
+// function loadShoppingList() {
+// const savedShoppingList = localStorage.getItem('shoppingList');
+// if (savedShoppingList) {
+// shoppingList = JSON.parse(savedShoppingList);
+// }
+// }
+
+
