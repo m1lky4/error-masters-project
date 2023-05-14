b2760803a8446e242d7ee5b663c42dd26afacfba
Vladyslav <124445034+VladyslavTodo@users.noreply.github.com>
2023-05-14 22:21:54 +0300

    Fire base (#36)
    * add-some-firebase
    
    * Але я втомився
    
    ---------
    
    Co-authored-by: Artem Kozlov <125802820+m1lky4@users.noreply.github.com>

--- src/js/firebase/auth.js
+++ src/js/firebase/auth.js
@@ -3,7 +3,6 @@
 //   console.log(app);
 //   try {
 //     await app.auth().createUserWithEmailAndPassword(email, username, password);
-
 //     const user = await app.auth().currentUser;
 //     await user.updateProfile({
 //       displayName: username,
@@ -17,66 +16,23 @@
 // }
 
 // createUserWithEmailAndPassword({
-//   email: 'f3@f.com',
-//   username: 'gergreg',
-//   password: 'gferager',
+//   email: 'sodglasen@f.com',
+//   username: 'gdergreg',
+//   password: 'gdferager',
 // });
 
 // async function signInWithEmailAndPassword({ email, password }) {
-// 	try {
-// 		await app.auth()
-// 			.signInWithEmailAndPassword(email, password)
-// 		// const email = app.email;
-
-// 	} catch (error) {
-// 		// console.log(error);
-// 	}
+//   try {
+//     await app.auth().signInWithEmailAndPassword(email, password);
+//     // const email = app.email;
+//   } catch (error) {
+//     // console.log(error);
+//   }
 // }
 
-// signInWithEmailAndPassword({
-//   email: 'f2@f.com',
-//   username: 'gergreg',
-//   password: 'gferager',
-// });
-// ===========
-import app from './firebase';
+// // signInWithEmailAndPassword({
+// //   email: 'f2@f.com',
+// //   username: 'gergreg',
+// //   password: 'gferager',
+// // });
 
-async function createUserWithEmailAndPassword({ email, username, password }) {
-  console.log(app);
-  try {
-    await app.auth().createUserWithEmailAndPassword(email, password);
-
-    const user = await app.auth().currentUser;
-    await user.updateProfile({
-      displayName: username,
-    });
-    console.log(user.displayName);
-    console.log(user.email);
-    console.log(user.uid);
-  } catch (error) {
-    console.log(error);
-    throw error; // Додатково кидаємо помилку для обробки вище
-  }
-}
-
-createUserWithEmailAndPassword({
-  email: 'komanda_v_nas@govno.com',
-  username: 'gergreg',
-  password: 'gferager',
-});
-
-async function signInWithEmailAndPassword({ email, password }) {
-  try {
-    await app.auth().signInWithEmailAndPassword(email, password);
-    const currentUser = app.auth().currentUser;
-    console.log(currentUser.email);
-  } catch (error) {
-    console.log(error);
-    throw error; // Додатково кидаємо помилку для обробки вище
-  }
-}
-
-signInWithEmailAndPassword({
-  email: 'f2@f.com',
-  password: 'gferager',
-});
