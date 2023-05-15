44cb2e60987368b68cd4ebbea5f86fd32a613a7e
Dezertirr <125862316+Dezertirr@users.noreply.github.com>
2023-05-15 18:14:06 +0300

    bugFix support

--- src/js/for-each-partial/support.js
+++ src/js/for-each-partial/support.js
@@ -1,26 +1,24 @@
-const buttonSlider = document.getElementById('support-slider')
-const buttonSliderUp = document.getElementById('support-slider-up')
-const supList = document.getElementById('support-list-id')
+const buttonSlider = document.getElementById('support-slider');
+const buttonSliderUp = document.getElementById('support-slider-up');
+const supList = document.getElementById('support-list-id');
 
+const liItemHeight = 59;
+let scrollHeight = 0;
 
-const lastItemIndex = 8;
-const firstItemIndex = 0
-
-function showNextFund (){
-    const lastItem = supList.children[lastItemIndex];
-    lastItem.scrollIntoView({ behavior: 'smooth' });
-    buttonSlider.style.display = 'none'
-    buttonSliderUp.style.display = 'flex'
-}
+function showNextFund() {
+    scrollHeight += liItemHeight
+    supList.scrollTop = scrollHeight;
+if (supList.scrollHeight - supList.clientHeight < scrollHeight) {
+    buttonSlider.style.display = 'none';
+    buttonSliderUp.style.display = 'flex';
+    scrollHeight = 0;
+}}
 
 function showFirstFund() {
-    const firstItem = supList.children[firstItemIndex];
-    firstItem.scrollIntoView({behavior:'smooth'})
+    supList.scrollTop = 0;
     buttonSlider.style.display = 'flex';
     buttonSliderUp.style.display = 'none';
 }
 
 buttonSlider.addEventListener('click', showNextFund);
-
-buttonSliderUp.addEventListener ('click',showFirstFund)
-
+buttonSliderUp.addEventListener('click', showFirstFund);
