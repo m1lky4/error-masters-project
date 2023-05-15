a747997a0d4053d7d2855df92de16587de0c674a
Maryna Bonko <103792395+Verlong@users.noreply.github.com>
2023-05-15 12:59:26 +0300

    Header (#39)
    * add html
    
    * markup and styling
    
    * switcher + menu modal draft
    
    * add class to index
    
    ---------
    
    Co-authored-by: Artem Kozlov <125802820+m1lky4@users.noreply.github.com>

---
+++ src/js/for-each-partial/header.js
@@ -0,0 +1,34 @@
+const themeSwitcher = document.getElementById('theme-switch');
+
+// light theme is default, that's why themeSwitcher is unchecked
+themeSwitcher.checked = true;
+setThemeOnLoad();
+themeSwitcher.addEventListener('change', clickHandler);
+
+function clickHandler() {
+  // if you change theme to dark, add 'dark' class and remove previus, add this change to local storage
+  // if you select light theme do the same work but add 'light' class
+  const currentClass = document.body.className;
+  console.log(currentClass);
+  if (currentClass === 'light') {
+    document.body.classList.remove('light');
+    document.body.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.body.classList.remove('dark');
+    document.body.classList.add('light');
+    localStorage.setItem('theme', 'light');
+  }
+}
+
+function setThemeOnLoad() {
+  // this helpful function should call every time when you update page, to check what theme was seted to local storage
+  // you should get data from LS and set saved theme to body
+  // dont forget about themeSwitcher, if current theme is dark, it should be checked
+  const savedTheme = localStorage.getItem('theme');
+  console.log(savedTheme);
+  document.body.className = savedTheme;
+  if (document.body.classList.contains('dark')) {
+    themeSwitcher.checked = false;
+  }
+}
