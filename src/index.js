e7278c47c9db959c395924e66ca658d64c693554
GIF1985 <125868361+GIF1985@users.noreply.github.com>
2023-05-16 12:20:13 +0300

    16-05-2023

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import './js/for-each-partial/book-list';
 import './js/for-each-partial/categories';
 import './js/for-each-partial/header';
 import './js/for-each-partial/modal';
-import './js/for-each-partial/store';
-// import './js/for-each-partial/pagination';
+
 import './js/for-each-partial/support';
 import './js/for-each-partial/book_list_markup'; //O.D.
 import './js/api/book.service';
