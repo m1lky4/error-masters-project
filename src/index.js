cbae490ffc754703bac243817566bb7d17df53d9
GIF1985 <125868361+GIF1985@users.noreply.github.com>
2023-05-16 10:39:27 +0300

    shopping - list js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './js/for-each-partial/categories';
 import './js/for-each-partial/header';
 import './js/for-each-partial/modal';
 import './js/for-each-partial/store';
+import './js/for-each-partial/pagination';
 import './js/for-each-partial/support';
 import './js/for-each-partial/book_list_markup'; //O.D.
 import './js/api/book.service';
@@ -13,4 +14,3 @@ import './js/for-each-partial/mobile-menu';
 
 // import './js/firebase/firebase';
 import './js/firebase/auth';
-
---
+++ src/js/for-each-partial/store.js
@@ -0,0 +1,104 @@
+import { BookAPI } from '../api/book.service';
+
+const bookApi = new BookAPI();
+
+let selectedBooks = {};
+
+const shoppingListArray =
+  JSON.parse(localStorage.getItem('shoppingList')) || [];
+console.log(shoppingListArray);
+const shoppingList = document.querySelector('.shopping-list');
+
+function addToFavorites(book) {
+  favoriteBooks.push(book);
+  localStorage.setItem('favoriteBooks', JSON.stringify(favoriteBooks));
+  renderFavorites();
+}
+
+function renderFavorites() {
+  if (!shoppingList) return;
+
+  shoppingList.innerHTML = '';
+
+  if (selectedBooks.length === 0) {
+    shoppingList.innerHTML = `
+      <p class="some-book">
+        This page is empty, add some books<br />and proceed to order.
+      </p>
+      <div class="stack-of-books">
+        <div class="stack-of-books">
+          <img
+            src="./images/books-list-00100.png"
+            alt="stack of books"
+            width="265"
+            height="198"
+          />
+        </div>
+      `;
+  } else {
+    shoppingList.innerHTML = selectedBooks
+      .map(book => {
+        return `
+          <li class="shopping-list-book">
+            <img class="shopping-book-img" src="${book.book_image}" alt="${book.title}" width="100" height="142" />
+            <div class="shopping-book-info">
+              <div class="shopping-book-title">
+                <h3>${book.title}</h3>
+                <p>${book.list_name}</p>
+              </div>
+              <a href="#">
+                <svg class="book__add-cart-icon" width="28" height="28">
+                  <use href="./images/blocks.svg#icon-blocks1"></use>
+                </svg>
+              </a>
+            </div>
+            <div class="shopping-book-box">
+              <p class="shopping-book-description">${book.description}</p>
+              <div class="shopping-book-author">
+                <p>${book.author}</p>
+                <ul class="shopping-book-retailers">
+                  <li class="books-retailers-icon">
+                    <a href="#">
+                      <svg class="amazon_logo-icon" width="32" height="14">
+                        <use href="./images/blocks.svg#icon-Amazon_logo"></use>
+                      </svg>
+                    </a>
+                  </li>
+                  <li class="books-retailers-icon">
+                    <a href="#">
+                      <svg class="icon-book-opened" width="20" height="20">
+                        <use href="./images/blocks.svg#icon-book-opened"></use>
+                      </svg>
+                    </a>
+                  </li>
+                  <li class="books-retailers-icon">
+                    <a href="#">
+                      <svg class="icon-books-cl" width="20" height="20">
+                        <use href="./images/blocks.svg#icon-books-cl"></use>
+                      </svg>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </li>
+        `;
+      })
+      .join('');
+  }
+}
+
+(async () => {
+  const books = await bookApi.getTopBooksList();
+  const selectedId = shoppingListArray.map(b => b.id);
+  selectedBooks = books.reduce((acc, b) => {
+    b.books.forEach(book => {
+      if (selectedId.includes(book._id)) {
+        acc.push(book);
+      }
+    });
+    return acc;
+  }, []);
+  console.log(selectedBooks);
+  renderFavorites();
+})();
