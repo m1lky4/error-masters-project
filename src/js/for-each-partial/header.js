e3398784230195ede9174d75cf2bb435235abea9
Maryna Bonko <bonko23@gmail.com>
2023-05-16 22:59:56 +0300

    fixed

--- src/js/for-each-partial/header.js
+++ src/js/for-each-partial/header.js
@@ -20,22 +20,25 @@ function clickHandler() {
 
 function setThemeOnLoad() {
   const savedTheme = localStorage.getItem('theme');
-  console.log(savedTheme);
+
   document.body.className = savedTheme;
   if (document.body.classList.contains('dark')) {
     themeSwitcher.checked = false;
   }
 }
-
-window.addEventListener('load', currenPage(window.location.pathname));
+///////////////////////////////////////////////////////////////////////////////////////
+window.addEventListener(
+  'load',
+  currenPage(window.location.pathname.split('/').pop())
+);
 
 function currenPage(location) {
   const homeLink = document.querySelector('.link-home');
   const shoppingLink = document.querySelector('.link-shoping');
-  if (location === '/index.html') {
+  if (location === 'index.html') {
     homeLink.classList.add('current');
     return;
-  } else if (location === '/shopping-list.html') {
+  } else if (location === 'shopping-list.html') {
     homeLink.classList.remove('current');
     shoppingLink.classList.add('current');
   }
