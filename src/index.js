d5ea0d1ee4a9e1a3611e3b1ea8bed8ff8e287574
Ch1mik <125959505+Ch1mik@users.noreply.github.com>
2023-05-16 10:31:53 +0200

    Footer + skeleton (#50)
    * Update _common.scss
    
    * first commit
    
    * swiper
    
    * comiite three
    
    * skeleton
    
    * done skeletone and footer

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ import './js/for-each-partial/store';
 import './js/for-each-partial/support';
 import './js/for-each-partial/book_list_markup'; //O.D.
 import './js/api/book.service';
+import './js/for-each-partial/footer-photo';
+import './js/for-each-partial/swiper-footer';
+import './js/for-each-partial/footer-modal';
 import './js/for-each-partial/create-markup-bookcard';
 import './js/for-each-partial/main';
+import './js/for-each-partial/shopping-skeleton';
 
 import './js/for-each-partial/mobile-menu';
 
