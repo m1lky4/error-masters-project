0b9cec1f740d1edca6aee732e05cbaa2d91e7e96
Сергій <125862316+Dezertirr@users.noreply.github.com>
2023-05-16 13:44:07 +0300

    Support upd (#53)
    * bugFix support
    
    * upd

--- src/js/for-each-partial/support.js
+++ src/js/for-each-partial/support.js
@@ -2,23 +2,25 @@ const buttonSlider = document.getElementById('support-slider');
 const buttonSliderUp = document.getElementById('support-slider-up');
 const supList = document.getElementById('support-list-id');
 
-const lastItemIndex = 8;
-const firstItemIndex = 0;
+
+const liItemHeight = 59;
+let scrollHeight = 0;
 
 function showNextFund() {
-  const lastItem = supList.children[lastItemIndex];
-  lastItem.scrollIntoView({ behavior: 'smooth' });
-  buttonSlider.style.display = 'none';
-  buttonSliderUp.style.display = 'flex';
-}
+    scrollHeight += liItemHeight
+    supList.scrollTop = scrollHeight;
+if (supList.scrollHeight - supList.clientHeight < scrollHeight) {
+    buttonSlider.style.display = 'none';
+    buttonSliderUp.style.display = 'flex';
+    scrollHeight = 0;
+}}
 
 function showFirstFund() {
-  const firstItem = supList.children[firstItemIndex];
-  firstItem.scrollIntoView({ behavior: 'smooth' });
-  buttonSlider.style.display = 'flex';
-  buttonSliderUp.style.display = 'none';
+    supList.scrollTop = 0;
+    buttonSlider.style.display = 'flex';
+    buttonSliderUp.style.display = 'none';
 }
 
 buttonSlider.addEventListener('click', showNextFund);
-
 buttonSliderUp.addEventListener('click', showFirstFund);
+
