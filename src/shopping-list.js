c417117e49670dc6a5d821362694e5639496a876
GIF1985 <125868361+GIF1985@users.noreply.github.com>
2023-05-17 21:18:43 +0300

    Gif (#79)
    * shopping-list
    
    * сделал под пк и планшет
    
    * добавил ссылку на shop list
    
    * +
    
    * +
    
    * +
    
    * +
    
    * +
    
    * +
    
    * +
    
    * +
    
    * shopping - list js
    
    * +
    
    * 16-05-2023
    
    * +
    
    * пагинация
    
    * +
    
    * картинка и скрыл пагинацию если нет книг на странице
    
    * +
    
    * 17-05-2023 рабочий
    
    * +

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,7 +1,7 @@
 import './js/for-each-partial/store';
-// import './js/for-each-partial/pagination';
+
 import './js/for-each-partial/header';
-import './js/for-each-partial/support';
+// import './js/for-each-partial/support';
 import './js/api/book.service';
 import './js/for-each-partial/mobile-menu';
 // import './js/firebase/firebase';
