33f3145a30fd9bdd1ca39783e50f88d61434d4e2
Oleksandr Dubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-05-17 18:01:41 +0300

    Firebase    (#78)
    * firebase
    
    * firebase
    
    * firebase
    
    * fireBase! done
    
    ---------
    
    Co-authored-by: Artem Kozlov <125802820+m1lky4@users.noreply.github.com>

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -5,6 +5,5 @@ import './js/for-each-partial/support';
 import './js/api/book.service';
 import './js/for-each-partial/mobile-menu';
 // import './js/firebase/firebase';
-import './js/firebase/auth';
 import './js/for-each-partial/registration-modal';
 import './js/for-each-partial/validation-registration-form';
